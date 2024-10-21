f070df4f3b341fbe95e2d1f15fb6a0d7ba00801f
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-10-21 12:12:07 +0700

    update rule for eslint

--- packages/config-eslint/react.js
+++ packages/config-eslint/react.js
@@ -44,6 +44,9 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    'no-console': 'off',
   },
   overrides: [
     {
