891959248f4d1062e0d158763c8a5680deaf2316
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-10-21 10:13:05 +0700

    config: update react any config

--- packages/config-eslint/react.js
+++ packages/config-eslint/react.js
@@ -42,6 +42,7 @@ module.exports = {
     'react/jsx-sort-props': 'off',
     'import/order': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   overrides: [
     {
