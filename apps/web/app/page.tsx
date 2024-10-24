813134fa7afcc3a5bd9af35a21b98d20483a01c8
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-10-24 15:31:48 +0700

    fix: page overflow

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import { SimpleChatSection, StyledChatSection } from './demo'
 
 export default function Page(): JSX.Element {
   return (
-    <main className="bg-glow-conic min-h-screen w-screen p-20">
+    <main className="bg-glow-conic min-h-screen p-20">
       <div className="mb-10 flex flex-col items-center justify-center space-y-4">
         <h1 className="text-6xl font-bold">LlamaIndex ChatUI</h1>
         <p className="text-2xl">Build powerful AI chat interfaces with ease</p>
