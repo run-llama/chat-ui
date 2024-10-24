67bbfdc651b4ea0a15e59905f7955d0547718d6e
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-10-24 09:10:52 +0700

    adjust example style

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,12 +2,12 @@ import { SimpleChatSection, StyledChatSection } from './chat-section'
 
 export default function Page(): JSX.Element {
   return (
-    <main className="bg-glow-conic w-screen min-h-screen p-20">
-      <div className="flex flex-col items-center justify-center space-y-4 mb-10">
+    <main className="bg-glow-conic min-h-screen w-screen p-20">
+      <div className="mb-10 flex flex-col items-center justify-center space-y-4">
         <h1 className="text-6xl font-bold">LlamaIndex ChatUI</h1>
         <p className="text-2xl">Build powerful AI chat interfaces with ease</p>
       </div>
-      <div className="w-[800px] space-y-8 mx-auto">
+      <div className="mx-auto w-[600px] space-y-8">
         <SimpleChatSection />
         <StyledChatSection />
       </div>
