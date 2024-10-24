a982f56619aa10fc0132d19294d3468fe3e1fa08
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-10-24 08:40:34 +0700

    make example simple

--- apps/nextjs-agent/app/components/chat-section.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ChatSection, useVercelAiSdk } from "@llamaindex/chat-ui";
-
-export default function ChatExample() {
-  const handler = useVercelAiSdk();
-  return <ChatSection handler={handler} />;
-}
