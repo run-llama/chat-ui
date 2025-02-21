ab9dd76450526a3780b040c975ae1df4ff6c53ae
thucpn <thucsh2@gmail.com>
2025-02-21 16:34:53 +0700

    migrate classname

--- apps/web/src/widgets/chat-image.tsx
+++
@@ -1,9 +0,0 @@
-import { type ImageData } from '../index'
-
-export function ChatImage({ data }: { data: ImageData }) {
-  return (
-    <div className="max-w-[200px] rounded-md shadow-md">
-      <img src={data.url} alt="chat_image" className="h-auto w-full" />
-    </div>
-  )
-}
