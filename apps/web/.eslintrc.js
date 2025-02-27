dea33b4be71a829a05cf6ffa433f8510f7d8d9e5
thucpn <thucsh2@gmail.com>
2025-02-27 09:18:53 +0700

    feat: build registry for chat-ui

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['@llamaindex/eslint-config/next.js'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-await-in-loop': 'off',
+    'no-promise-executor-return': 'off',
+    '@typescript-eslint/no-loop-func': 'off',
+  },
 }
