696c7f08b4cf9e6a77057a558702389b04266fb4
thucpn <thucsh2@gmail.com>
2025-03-26 11:21:26 +0700

    enhance UI of source nodes

--- packages/chat-ui/src/widgets/document-info.tsx
+++ packages/chat-ui/src/widgets/document-info.tsx
@@ -43,13 +43,13 @@ export function DocumentInfo({
   }
 
   const DocumentDetail = (
-    <div className={`relative ${className}`}>
+    <div className={`bg-secondary rounded-lg p-2 ${className}`}>
       <DocumentPreviewCard
         className="cursor-pointer"
         file={previewFile}
         onRemove={onRemove}
       />
-      <div className="absolute bottom-2 right-2 flex space-x-2">
+      <div className="flex space-x-2 max-w-60 flex-wrap px-2">
         {sources.map((node: SourceNode, index: number) => (
           <div key={node.id}>
             <SourceInfo node={node} index={startIndex + index} />
