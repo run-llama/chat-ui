78c31fe9c2ee6031d463151bfa56563c8fb1257b
thucpn <thucsh2@gmail.com>
2025-03-26 12:02:26 +0700

    fix starter questions UI

--- packages/chat-ui/src/widgets/starter-questions.tsx
+++ packages/chat-ui/src/widgets/starter-questions.tsx
@@ -9,12 +9,13 @@ interface StarterQuestionsProps {
 export function StarterQuestions(props: StarterQuestionsProps) {
   return (
     <div className="absolute bottom-6 left-0 w-full">
-      <div className="mx-20 grid grid-cols-2 gap-2">
+      <div className="mx-10 grid grid-cols-2 gap-2">
         {props.questions.map((question, i) => (
           <Button
             key={i}
             variant="outline"
             onClick={() => props.append({ role: 'user', content: question })}
+            className="h-auto whitespace-break-spaces"
           >
             {question}
           </Button>
