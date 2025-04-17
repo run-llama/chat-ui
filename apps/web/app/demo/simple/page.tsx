58b5c898898269c56fa73d7c57631c281e63a584
thucpn <thucsh2@gmail.com>
2025-04-17 18:09:42 +0700

    remove initial messages in simple demo

--- apps/web/app/demo/simple/page.tsx
+++ apps/web/app/demo/simple/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Message, useChat } from 'ai/react'
+import { useChat } from 'ai/react'
 import { ChatSection } from '@llamaindex/chat-ui'
 import { Code } from '@/components/code'
 
@@ -14,34 +14,8 @@ function SimpleChat() {
 }
 `
 
-const initialMessages: Message[] = [
-  {
-    id: '1',
-    content: 'Write simple Javascript hello world code',
-    role: 'user',
-  },
-  {
-    id: '2',
-    role: 'assistant',
-    content:
-      'Got it! Here\'s the simplest JavaScript code to print "Hello, World!" to the console:\n\n```javascript\nconsole.log("Hello, World!");\n```\n\nYou can run this code in any JavaScript environment, such as a web browser\'s console or a Node.js environment. Just paste the code and execute it to see the output.',
-  },
-  {
-    id: '3',
-    content:
-      'Write a simple math equation that solves for x. It should be a quadratic equation. Please use LaTeX to write the equation.',
-    role: 'user',
-  },
-  {
-    id: '4',
-    role: 'assistant',
-    content:
-      "Let's explore a simple mathematical equation using LaTeX:\n\n The quadratic formula is: $$x = \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}$$\n\nThis formula helps us solve quadratic equations in the form $ax^2 + bx + c = 0$. The solution gives us the x-values where the parabola intersects the x-axis.",
-  },
-]
-
 export default function Page(): JSX.Element {
-  const handler = useChat({ initialMessages })
+  const handler = useChat()
   return (
     <div className="flex gap-10">
       <div className="w-1/3 justify-center space-y-10 self-center p-10">
