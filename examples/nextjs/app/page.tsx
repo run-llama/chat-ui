b892d9319a209ea6ce849aed11e46a2c238e5cdf
thucpn <thucsh2@gmail.com>
2025-05-30 15:56:42 +0700

    fix: lint

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -41,9 +41,9 @@ export default function Page(): JSX.Element {
 
 function ChatExample() {
   const handler = useChat({
-    // api: '/api/chat',
+    api: '/api/chat',
     // uncomment this to try the advanced example about inline annotations (app/api/chat/advanced/route.ts)
-    api: '/api/chat/advanced',
+    // api: '/api/chat/advanced',
     initialMessages,
   })
 
