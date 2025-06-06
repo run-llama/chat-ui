fffed17af11d6dc2a0cd36b2e18831600a9d5e2c
thucpn <thucsh2@gmail.com>
2025-06-06 17:05:35 +0700

    add edge example

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -40,9 +40,14 @@ export default function Page(): JSX.Element {
 
 function ChatExample() {
   const handler = useChat({
-    // api: '/api/chat',
+    api: '/api/chat',
+
+    // uncomment this to try edge runtime example in app/api/chat/edge/route.ts
+    // api: '/api/chat/edge',
+
     // uncomment this to try advanced example in app/api/chat/advanced/route.ts
-    api: '/api/chat/advanced',
+    // api: '/api/chat/advanced',
+
     initialMessages,
   })
 
