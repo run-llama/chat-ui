3a366d71ea7adec9450d4ee1f67487312b0fa488
thucpn <thucsh2@gmail.com>
2025-06-24 09:30:57 +0700

    add hitl workflow example

---
+++ examples/llama-deploy/src/hitl_workflow.py
@@ -0,0 +1,50 @@
+import asyncio
+from typing import Literal
+
+from llama_index.core.workflow import (
+    Workflow,
+    StartEvent,
+    StopEvent,
+    step,
+    Event,
+    HumanResponseEvent,
+    InputRequiredEvent,
+)
+from pydantic import Field
+
+
+class EchoEvent(Event):
+    """An event that echoes the input message."""
+
+    message: str = Field(description="The message to echo.")
+
+
+class WaitEvent(InputRequiredEvent):
+    wait_please: Literal[True]
+
+
+class ContinueEvent(HumanResponseEvent):
+    """An event that continues the workflow."""
+
+    continue_please: Literal[True]
+
+
+# create a dummy workflow
+class EchoWorkflow(Workflow):
+    """A dummy workflow with only one step sending back the input given."""
+
+    def __init__(self, *args, **kwargs):
+        if "timeout" not in kwargs:
+            kwargs["timeout"] = None
+        super().__init__(*args, **kwargs)
+
+    @step
+    async def step1(self, ev: StartEvent) -> InputRequiredEvent:
+        return InputRequiredEvent(prefix="Enter a number: ")
+
+    @step
+    async def step2(self, ev: HumanResponseEvent) -> StopEvent:
+        return StopEvent(result=ev.response)
+
+
+workflow = EchoWorkflow()
