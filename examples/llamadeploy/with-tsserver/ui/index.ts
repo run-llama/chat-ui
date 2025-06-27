8349534db07a4d9d020f8f70e1fecfd7c329e551
thucpn <thucsh2@gmail.com>
2025-06-27 16:58:58 +0700

    add simple server

---
+++ examples/llamadeploy/with-tsserver/ui/index.ts
@@ -0,0 +1,16 @@
+import { LlamaIndexServer } from '@llamaindex/server'
+
+new LlamaIndexServer({
+  workflow: () => ({}) as any, // TODO: update ts server to not require a workflow
+  uiConfig: {
+    starterQuestions: ['What can you do?', 'what is the weather in tokyo?'],
+    componentsDir: 'components',
+    layoutDir: 'layouts',
+    // deploymentName: 'chat', // TODO
+    // workflowName: 'workflow', // TODO
+    // devMode: false, // TODO: disable dev mode because workflow is in python
+    // enableFileUpload: true, // TODO: support file upload in nextjs
+    // llamaCloudIndexSelector: true, // TODO: support llamacloud
+  },
+  port: 3000,
+}).start()
