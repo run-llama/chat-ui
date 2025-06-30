6b94ff3ba616e8c1cfccb7e8f61d152a238aae4a
thucpn <thucsh2@gmail.com>
2025-06-30 11:55:25 +0700

    with proxy

--- examples/llamadeploy/with-tsserver/ui/index.ts
+++ examples/llamadeploy/with-tsserver/ui/index.ts
@@ -1,13 +1,14 @@
 import { LlamaIndexServer } from '@llamaindex/server'
 
 new LlamaIndexServer({
-  workflow: () => ({}) as any, // TODO: update ts server to not require a workflow
   uiConfig: {
     starterQuestions: ['What can you do?', 'what is the weather in tokyo?'],
     componentsDir: 'components',
     layoutDir: 'layout',
-    // deploymentName: 'chat', // TODO
-    // workflowName: 'workflow', // TODO
+    llamaDeploy: {
+      deployment: 'chat',
+      workflow: 'workflow',
+    },
     // devMode: false, // TODO: disable dev mode because workflow is in python
     // enableFileUpload: true, // TODO: support file upload in nextjs
     // llamaCloudIndexSelector: true, // TODO: support llamacloud
