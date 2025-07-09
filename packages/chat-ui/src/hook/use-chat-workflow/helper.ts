62955a6a188392eb8ac82ba4b69e024aadefbf21
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2025-07-09 14:41:50 +0700

    fix(useWorkflow): handle incomplete chunk and support tool call transformation (#159)
    * fix: handle fail to parse chunk
    
    * add test
    
    * Create gorgeous-lies-develop.md
    
    * support transforming toolcall and toolcalloutput
    
    * raw node to source node
    
    * fileServerUrl option for useChatWorkflow
    
    * add normal agentic rag example
    
    * missing file
    
    * return slug url
    
    * update pnpm workspace
    
    * fix format
    
    * feat: add agentic rag cloud example
    
    * fix format
    
    * remove dead files
    
    * fix data
    
    * RawNodeWithScore
    
    * update url
    
    * add comment
    
    * log info
    
    * update llamacloud example

--- packages/chat-ui/src/hook/use-chat-workflow/helper.ts
+++ packages/chat-ui/src/hook/use-chat-workflow/helper.ts
@@ -5,8 +5,15 @@ import {
 } from '../../chat/annotations'
 import { JSONValue } from '../../chat/chat.interface'
 import { WorkflowEvent, WorkflowEventType } from '../use-workflow'
-import { AgentStreamEvent, SourceNodesEvent, UIEvent } from './types'
-import { SourceNode } from '../../widgets'
+import {
+  AgentStreamEvent,
+  RawNodeWithScore,
+  SourceNodesEvent,
+  ToolCallEvent,
+  ToolCallResultEvent,
+  UIEvent,
+} from './types'
+import { AgentEventData, SourceNode } from '../../widgets'
 
 /**
  * Transform a workflow event to message parts
@@ -16,7 +23,10 @@ import { SourceNode } from '../../widgets'
  * @param event - The event to transform
  * @returns The message parts (delta and annotations)
  */
-export function transformEventToMessageParts(event: WorkflowEvent): {
+export function transformEventToMessageParts(
+  event: WorkflowEvent,
+  fileServerUrl?: string
+): {
   delta: string
   annotations: MessageAnnotation<JSONValue>[]
 } {
@@ -31,7 +41,7 @@ export function transformEventToMessageParts(event: WorkflowEvent): {
     }
   }
 
-  const annotations = toVercelAnnotations(event)
+  const annotations = toVercelAnnotations(event, fileServerUrl)
   return { delta: '', annotations }
 }
 
@@ -51,7 +61,7 @@ function isInlineEvent(event: WorkflowEvent) {
   return inlineEventTypes.includes(event.type) && hasInlineData
 }
 
-function toVercelAnnotations(event: WorkflowEvent) {
+function toVercelAnnotations(event: WorkflowEvent, fileServerUrl?: string) {
   switch (event.type) {
     // convert source nodes event to source nodes annotation
     case WorkflowEventType.SourceNodesEvent.toString(): {
@@ -64,18 +74,14 @@ function toVercelAnnotations(event: WorkflowEvent) {
         return []
       }
 
-      const sources = nodes.map(({ node, score }) => ({
-        id: node.id_,
-        metadata: node.metadata,
-        score,
-        text: node.text,
-        url: (node.metadata?.URL as string) || '',
-      })) satisfies SourceNode[]
-
       return [
         {
           type: MessageAnnotationType.SOURCES,
-          data: { nodes: sources },
+          data: {
+            nodes: nodes.map(rawNode =>
+              convertRawNodeToSourceNode(rawNode, fileServerUrl)
+            ),
+          },
         },
       ]
     }
@@ -91,6 +97,72 @@ function toVercelAnnotations(event: WorkflowEvent) {
       ]
     }
 
+    // transfrom ToolCallEvent to AgentEvent
+    case WorkflowEventType.ToolCall.toString(): {
+      const { data } = event as ToolCallEvent
+
+      if (
+        'tool_name' in data &&
+        'tool_kwargs' in data &&
+        typeof data.tool_kwargs === 'object'
+      ) {
+        const { tool_name, tool_kwargs } = data
+
+        return [
+          {
+            type: MessageAnnotationType.AGENT_EVENTS,
+            data: {
+              agent: 'Agent',
+              text: `Calling tool: ${tool_name} with: ${JSON.stringify(tool_kwargs)}`,
+            } as AgentEventData,
+          },
+        ]
+      }
+
+      console.warn(
+        `Not supported this tool call event: ${JSON.stringify(event)}`
+      )
+
+      return []
+    }
+
+    // transform ToolCallResultEvent to suitable annotations
+    case WorkflowEventType.ToolCallResult.toString(): {
+      const { data } = event as ToolCallResultEvent
+      const { tool_output } = data
+
+      if (
+        'raw_output' in tool_output &&
+        typeof tool_output.raw_output === 'object' &&
+        tool_output.raw_output !== null
+      ) {
+        const { raw_output } = tool_output
+
+        // to source nodes annotation
+        if (
+          'source_nodes' in raw_output &&
+          Array.isArray(raw_output.source_nodes)
+        ) {
+          const rawNodes = raw_output.source_nodes as RawNodeWithScore[]
+          return [
+            {
+              type: MessageAnnotationType.SOURCES,
+              data: {
+                nodes: rawNodes.map(rawNode =>
+                  convertRawNodeToSourceNode(rawNode, fileServerUrl)
+                ),
+              },
+            },
+          ]
+        }
+      }
+
+      console.warn(
+        `Not supported this tool call result event: ${JSON.stringify(event)}`
+      )
+      return []
+    }
+
     // for other events which are not defined, convert them to vercel annotations with type is the qualified name of the event
     // eg: {"__is_pydantic": true, "value": {"item": "sample"}, "qualified_name": "myworkflow.MyEvent"}
     // will be converted to this annotation: {"type": "myworkflow.MyEvent", "data": {"item": "sample"}}
@@ -104,4 +176,55 @@ function toVercelAnnotations(event: WorkflowEvent) {
       ]
     }
   }
+}
+
+function convertRawNodeToSourceNode(
+  rawNode: RawNodeWithScore,
+  fileServerUrl?: string
+): SourceNode {
+  const { node, score } = rawNode
+
+  return {
+    id: node.id_,
+    metadata: node.metadata,
+    score,
+    text: node.text,
+    url: getDocumentUrlFromRawNode(rawNode, fileServerUrl),
+  }
+}
+
+function getDocumentUrlFromRawNode(
+  rawNode: RawNodeWithScore,
+  fileServerUrl?: string
+): string {
+  const { metadata } = rawNode.node
+  const { URL: fileURL, pipeline_id } = metadata
+
+  // if the file URL is provided in the metadata, use it
+  if (fileURL) return fileURL
+
+  // if `fileServerUrl` is not provided, return empty string
+  if (!fileServerUrl) {
+    console.warn(
+      `No 'fileServerUrl' provided. Nodes won't be displayed in ChatSources UI.`
+    )
+    return ''
+  }
+
+  let fileName = metadata?.file_name
+
+  // check if file_name is provided in the metadata
+  if (!fileName) {
+    console.warn(
+      `No file name found in this raw node: ${JSON.stringify(rawNode)}. It won't be displayed in ChatSources UI.`
+    )
+    return ''
+  }
+
+  if (pipeline_id) {
+    // pipeline_id is provided, the file is stored in LlamaCloud, make sure to cache it at the `fileServerUrl`
+    fileName = `${pipeline_id}$${fileName}`
+  }
+
+  return `${fileServerUrl}/${fileName}`
 }
--- packages/chat-ui/src/hook/use-chat-workflow/types.ts
+++ packages/chat-ui/src/hook/use-chat-workflow/types.ts
@@ -20,18 +20,30 @@ export interface AgentStreamEvent extends WorkflowEvent {
   }
 }
 
+// represent the raw node in the source nodes event
+export type RawNodeWithScore = {
+  node: {
+    id_: string
+    metadata: {
+      file_name: string | null
+      pipeline_id: string | null
+      pipeline_file_id: string | null
+      page_label: string | null
+      file_path: string | null
+      file_type: string | null
+      file_size: number | null
+      creation_date: string | null
+      last_modified_date: string | null
+      URL: string | null
+    }
+    text: string
+  }
+  score: number
+}
+
 export interface SourceNodesEvent extends WorkflowEvent {
   type: WorkflowEventType.SourceNodesEvent
-  data: {
-    nodes: {
-      node: {
-        id_: string
-        metadata: Record<string, JSONValue>
-        text: string
-      }
-      score: number
-    }[]
-  }
+  data: { nodes: RawNodeWithScore[] }
 }
 
 export interface UIEvent extends WorkflowEvent {
@@ -42,10 +54,37 @@ export interface UIEvent extends WorkflowEvent {
   }
 }
 
+export interface ToolCallEvent extends WorkflowEvent {
+  type: WorkflowEventType.ToolCall
+  data: {
+    tool_id: string
+    tool_name: string
+    tool_kwargs: JSONValue
+  }
+}
+
+export interface ToolCallResultEvent extends WorkflowEvent {
+  type: WorkflowEventType.ToolCallResult
+  data: {
+    tool_id: string
+    tool_name: string
+    tool_kwargs: JSONValue
+    tool_output: {
+      raw_output: JSONValue
+    }
+  }
+}
+
 export type ChatWorkflowHookParams = Pick<
   WorkflowHookParams,
   'deployment' | 'workflow' | 'baseUrl' | 'onError'
->
+> & {
+  // the endpoint to serve local files or llamacloud files
+  // if not provided, url for files cannot be constructed, and ChatSources won't be displayed in the UI
+  // eg. for non-llamacloud: localhost:3000/deployments/chat/ui/api/files/data
+  // eg. for llamacloud: localhost:3000/deployments/chat/ui/api/files/output/llamacloud
+  fileServerUrl?: string
+}
 
 export type ChatWorkflowHookHandler = ChatHandler & {
   resume: ChatWorkflowResume
