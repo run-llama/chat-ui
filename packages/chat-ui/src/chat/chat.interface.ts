901f04a64a1c27c0e0b46134b9ee1686408798a3
thucpn <thucsh2@gmail.com>
2025-07-29 10:24:57 +0700

    update comment

--- packages/chat-ui/src/chat/chat.interface.ts
+++ packages/chat-ui/src/chat/chat.interface.ts
@@ -20,6 +20,8 @@ export type MessagePart = {
 }
 
 // User can have various parts, so we need to allow any parts when updating or sending messages
+// See many specific MessagePart types from Vercel AI SDK here:
+// https://github.com/vercel/ai/blob/7948ec215d21675c1100edf58af8bb03a1f1dbe4/packages/ai/src/ui/ui-messages.ts#L75-L272
 export type MessageInput = Omit<Message, 'parts'> & { parts: any[] }
 
 export type ChatRequestOptions = {
@@ -37,7 +39,7 @@ export type ChatHandler = {
 }
 
 export type ChatContext = ChatHandler & {
-  // input state
+  // user input state
   input: string
   setInput: (input: string) => void
 
