ff0a2817ca1930cbe902a6d03430ed7cd259b9d2
thucpn <thucsh2@gmail.com>
2025-08-05 08:28:32 +0700

    enhance group parts

--- packages/chat-ui/src/chat/message-parts/render.tsx
+++ packages/chat-ui/src/chat/message-parts/render.tsx
@@ -3,7 +3,15 @@ import { MessagePart } from '../chat.interface'
 import { EventsPart, EventsPartProps } from './parts/events'
 import { ImagePart, ImagePartProps } from './parts/image'
 
-export interface IMessagePartUI<P = any> {
+/**
+ * Configuration for rendering a part inside a message
+ * @param type - The type of the part, can be `text` or `data-${string}`
+ * @param component - The React component to render the part
+ * @param props - The initial props of the part, will be passed to the component when rendering
+ * @param position - The position of the part, can be `auto`, `top` or `bottom` (default: `auto` which means default position of the part)
+ * @param group - Whether the part is grouped, if true, the part will be grouped with other parts of the same type
+ */
+export interface PartUI<P = any> {
   type: 'text' | `data-${string}`
   props?: P
   component: ComponentType<P>
@@ -11,9 +19,9 @@ export interface IMessagePartUI<P = any> {
   group?: boolean
 }
 
-export type MessagePartFactory<P = any> = (props?: P) => IMessagePartUI<P>
+export type PartFactory<P = any> = (props?: P) => PartUI<P>
 
-export const image: MessagePartFactory<ImagePartProps> = props => {
+export const image: PartFactory<ImagePartProps> = props => {
   return {
     type: 'data-image',
     props,
@@ -21,7 +29,7 @@ export const image: MessagePartFactory<ImagePartProps> = props => {
   }
 }
 
-export const events: MessagePartFactory<EventsPartProps> = props => {
+export const events: PartFactory<EventsPartProps> = props => {
   return {
     group: true,
     type: 'data-events',
@@ -30,12 +38,60 @@ export const events: MessagePartFactory<EventsPartProps> = props => {
   }
 }
 
-export const configs: IMessagePartUI[] = [image(), events()]
+export const configs: PartUI[] = [image(), events()]
 
 const map = Object.fromEntries(configs.map(config => [config.type, config]))
 
-// eg: [text, event 1, image, event 2, text 2] and events is grouped
-// -> [[text 1], [event 1, event 2], [image], [text 2]]
+const classNameMap: Record<string, string> = {
+  top: 'order-first', // tailwind class to make the part at the top in flexbox
+  bottom: 'order-last', // tailwind class to make the part at the bottom in flexbox
+}
+
+export function renderParts(parts: MessagePart[]): React.ReactNode[] {
+  const supportedParts = parts.filter(part => map[part.type])
+  const groupedParts = groupParts(supportedParts)
+
+  return groupedParts.map((group, index) => {
+    if (group.length === 1) {
+      // Single part, render as a component with part prop
+      // Eg: <TextPart part={part} />
+      const part = group[0]
+      const config = map[part.type]
+      if (!config) return null // not supported part, skip
+
+      const { position, props, component: Component } = config
+
+      const componentProps = {
+        ...props,
+        className: position ? classNameMap[position] : undefined,
+        part, // passing `part` for a single part
+      }
+
+      return <Component key={index} {...componentProps} />
+    } else {
+      // Multiple parts of the same type, render as a component with parts prop
+      // Eg: <SourcesPart parts={parts} />
+      const partType = group[0].type
+      const config = map[partType]
+      if (!config) return null // not supported part, skip
+
+      const { position, props, component: Component } = config
+
+      const componentProps = {
+        ...props,
+        className: position ? classNameMap[position] : undefined,
+        parts: group, // passing `parts` for multiple parts
+      }
+
+      return <Component key={index} {...componentProps} />
+    }
+  })
+}
+
+// Group parts by type, if the type is marked as grouped
+// This is useful for parts that need to be grouped together, like sources
+// Input: [text 1, source 1, image 1, source 2, text 2], sources is marked as grouped
+// Output: [[text 1], [source 1, source 2], [image 1], [text 2]]
 function groupParts(parts: MessagePart[]): MessagePart[][] {
   const groups: MessagePart[][] = []
   const existedGroupTypes = new Set<string>()
@@ -57,41 +113,4 @@ function groupParts(parts: MessagePart[]): MessagePart[][] {
   }
 
   return groups
-}
-
-const classNameMap: Record<string, string> = {
-  top: 'order-first',
-  bottom: 'order-last',
-}
-
-export function render(parts: MessagePart[]): React.ReactNode[] {
-  const supportedParts = parts.filter(part => map[part.type])
-  const groupedParts = groupParts(supportedParts)
-
-  return groupedParts.map((group, index) => {
-    if (group.length === 1) {
-      const part = group[0]
-      const config = map[part.type]
-      if (!config) return null
-
-      const { position, props, component: Component } = config
-      const className = position ? classNameMap[position] : undefined
-
-      return (
-        <Component key={index} {...props} className={className} part={part} />
-      )
-    } else {
-      // Multiple parts of the same type, render as a group
-      const partType = group[0].type
-      const config = map[partType]
-      if (!config) return null
-
-      const { position, props, component: Component } = config
-      const className = position ? classNameMap[position] : undefined
-
-      return (
-        <Component key={index} {...props} className={className} parts={group} />
-      )
-    }
-  })
 }
