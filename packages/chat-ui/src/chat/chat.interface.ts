9da31c740a0aebb789c5f978f55570b3490701f6
thucpn <thucsh2@gmail.com>
2025-08-06 10:36:30 +0700

    support AnyPart

--- packages/chat-ui/src/chat/chat.interface.ts
+++ packages/chat-ui/src/chat/chat.interface.ts
@@ -15,11 +15,14 @@ export interface Message {
 }
 
 /**
- * Chat-UI MessagePart contract, have two types of parts:
+ * Chat-UI MessagePart contract, have 3 types of parts:
  * - text parts: will be rendered as one or multiple markdown blocks
  * - data parts: additional data blocks
+ * - other parts: the remaining parts that are not text or data
+ *
+ * Note that user can have various types of parts. In ChatUI, by default we only support display text and data parts, other parts will be ignored.
  */
-export type MessagePart = TextPart | DataPart
+export type MessagePart = TextPart | DataPart | AnyPart
 
 export const TextPartType = 'text' as const
 
@@ -30,16 +33,17 @@ export type TextPart = {
 
 export type DataPart = {
   id?: string // if id is provided, only last data part with the same id will be existed in message.parts
-  type: string
-  data?: any
+  type: `data-${string}` // `data-` prefix is required for data parts
+  data: any
 }
 
-// User can have various parts, so we need to allow any parts when updating or sending messages
+// User can have various parts, so we need to allow any parts
 // See many specific MessagePart types from Vercel AI SDK here:
 // https://github.com/vercel/ai/blob/7948ec215d21675c1100edf58af8bb03a1f1dbe4/packages/ai/src/ui/ui-messages.ts#L75-L272
-export type MessageInput = Omit<Message, 'parts'> & {
-  parts: any[]
+export type AnyPart<T extends string = any> = {
   id?: string
+  type: T
+  data?: any
 }
 
 export type ChatRequestOptions = {
@@ -50,10 +54,10 @@ export type ChatRequestOptions = {
 export type ChatHandler = {
   messages: Message[]
   status: 'submitted' | 'streaming' | 'ready' | 'error'
-  sendMessage: (msg: MessageInput, opts?: ChatRequestOptions) => Promise<void>
+  sendMessage: (msg: Message, opts?: ChatRequestOptions) => Promise<void>
   stop?: () => Promise<void>
   regenerate?: (opts?: { messageId?: string } & ChatRequestOptions) => void
-  setMessages?: (messages: MessageInput[]) => void
+  setMessages?: (messages: Message[]) => void
 }
 
 export type ChatContext = ChatHandler & {
--- packages/chat-ui/src/chat/message-parts/context.ts
+++ packages/chat-ui/src/chat/message-parts/context.ts
@@ -16,13 +16,31 @@ export const chatPartContext = createContext<ChatPartContext | null>(null)
 export const ChatPartProvider = chatPartContext.Provider
 
 /**
- * Get the current part data.
- * Return text if the part type is text.
- * Return data if the part type is data.
+ * Get the current part. Return null if the input type is not matched with current part type.
+ */
+export const useCurrentPart = <T>(
+  type: 'text' | `data-${string}` | string
+): T | null => {
+  const context = useContext(chatPartContext)
+  if (!context) {
+    throw new Error('usePart must be used within a ChatPartProvider')
+  }
+
+  if (context.part.type !== type) return null // part type not matched
+  return context.part as T // return current part
+}
+
+/**
+ * Get the current part data by the given type.
  * Return null if the input type is not matched with current part type.
+ * By default, it will extract text or data part data only.
+ * You can provide a custom extract function to extract other part data.
  */
 export const usePartData = <T>(
-  partType: 'text' | `data-${string}`
+  type: string,
+  extractData: (
+    part: MessagePart
+  ) => T | null = extractDataFromTextPartOrDataPart
 ): T | null => {
   const context = useContext(chatPartContext)
 
@@ -30,42 +48,55 @@ export const usePartData = <T>(
     throw new Error('usePartData must be used within a ChatPartProvider')
   }
 
-  if (context.part.type !== partType) {
-    return null
-  }
+  if (context.part.type !== type) return null // part type not matched
 
-  if (partType === TextPartType) {
-    return (context.part as TextPart).text as T
-  }
-
-  if ('data' in context.part) {
-    return (context.part as DataPart).data as T
-  }
-
-  return null
+  return extractData(context.part)
 }
 
 /**
  * Extract all part data from a message by part type.
  * This is useful when you want to accumulate data from all parts.
+ * By default, it will extract text or data part data only.
+ * You can provide a custom extract function to extract other part data.
  * Eg: get all artifacts to display history, get all sources to display markdown citations.
  */
 export function extractAllPartData<T>(
   message: Message,
-  partType: 'text' | `data-${string}`
+  partType: 'text' | `data-${string}`,
+  extractData: (
+    part: MessagePart
+  ) => T | null = extractDataFromTextPartOrDataPart
 ): T[] {
   return message.parts
     .filter(part => part.type === partType)
-    .map(part => {
-      if (part.type === TextPartType) {
-        return (part as TextPart).text
-      }
-
-      if ('data' in part) {
-        return (part as DataPart).data
-      }
-
-      return null
-    })
+    .map(extractData)
     .filter(Boolean) as T[]
+}
+
+/**
+ * Extract data from text or data part.
+ * Return text content if the part type is text.
+ * Return data content if the part type is data.
+ * Return null if the part type is not text or data.
+ */
+export function extractDataFromTextPartOrDataPart<T>(
+  part: MessagePart
+): T | null {
+  if (isTextPart(part)) return part.text as T
+  if (isDataPart(part)) return part.data as T
+  return null
+}
+
+/**
+ * Data part must have type starts with `data-` and have data property.
+ */
+export function isDataPart(part: MessagePart): part is DataPart {
+  return part.type.startsWith('data-') && 'data' in part
+}
+
+/**
+ * Text part must have type `text` and have text property.
+ */
+export function isTextPart(part: MessagePart): part is TextPart {
+  return part.type === TextPartType && 'text' in part
 }
