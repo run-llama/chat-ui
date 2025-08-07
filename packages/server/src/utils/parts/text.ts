b01e02936494d2c6af9a752e3f64a667666f82fe
thucpn <thucsh2@gmail.com>
2025-08-07 18:05:32 +0700

    new streaming logic

--- packages/server/src/utils/parts/text.ts
+++ packages/server/src/utils/parts/text.ts
@@ -1,23 +1,71 @@
-import { agentStreamEvent, type WorkflowEventData } from '@llamaindex/workflow'
+import { randomUUID } from '@llamaindex/env'
+import {
+  agentStreamEvent,
+  workflowEvent,
+  type WorkflowEventData,
+} from '@llamaindex/workflow'
 import type { ChatResponseChunk } from 'llamaindex'
 
-export async function writeResponseToStream(
+export const TEXT_START_PART_TYPE = 'text-start'
+export const TEXT_DELTA_PART_TYPE = 'text-delta'
+export const TEXT_END_PART_TYPE = 'text-end'
+
+export type TextStartPart = {
+  type: typeof TEXT_START_PART_TYPE
+  id: string
+}
+
+export type TextDeltaPart = {
+  type: typeof TEXT_DELTA_PART_TYPE
+  id: string
+  delta: string
+}
+
+export type TextEndPart = {
+  type: typeof TEXT_END_PART_TYPE
+  id: string
+}
+
+export const textStartEvent = workflowEvent<TextStartPart>() // this event must be triggered before streaming text
+export const textDeltaEvent = workflowEvent<TextDeltaPart>() // equal to agentStreamEvent but using TextDeltaPart format
+export const textEndEvent = workflowEvent<TextEndPart>() // this event must be triggered after streaming text
+
+/**
+ * Send text events to stream, finish when generator is done
+ * @param generator - AsyncIterable<ChatResponseChunk<object>>
+ * @param sendEvent - (event: WorkflowEventData<unknown>) => void
+ * @returns the final response text
+ */
+export async function streamText(
   generator: AsyncIterable<ChatResponseChunk<object>>,
   sendEvent: (event: WorkflowEventData<unknown>) => void
 ) {
   let response = ''
+
   if (generator) {
+    // each message can have multiple text parts, so we need to use a unique id for each text part
+    const textPartId = randomUUID()
+
+    // send a start event for this text part
+    sendEvent(
+      textStartEvent.with({ id: textPartId, type: TEXT_START_PART_TYPE })
+    )
+
+    // consume generator and send delta events
     for await (const chunk of generator) {
       response += chunk.delta
       sendEvent(
-        agentStreamEvent.with({
+        textDeltaEvent.with({
+          id: textPartId,
+          type: TEXT_DELTA_PART_TYPE,
           delta: chunk.delta,
-          response,
-          currentAgentName: 'LLM',
-          raw: chunk.raw,
         })
       )
     }
+
+    // send an end event for this text part
+    sendEvent(textEndEvent.with({ id: textPartId, type: TEXT_END_PART_TYPE }))
   }
+
   return response
 }
--- packages/server/src/utils/workflow.ts
+++ packages/server/src/utils/workflow.ts
@@ -1,4 +1,5 @@
 import {
+  agentStreamEvent,
   agentToolCallEvent,
   agentToolCallResultEvent,
   startAgentEvent,
@@ -6,6 +7,7 @@ import {
   type AgentInputData,
   type Workflow,
   type WorkflowContext,
+  type WorkflowEvent,
   type WorkflowEventData,
 } from '@llamaindex/workflow'
 import {
@@ -17,14 +19,20 @@ import {
   runEvent,
   sourceEvent,
   toSourceEvent,
+  type EventPart,
   type SourceEventNode,
+  type TextDeltaPart,
 } from './parts'
 import { downloadFile } from './file'
 import {
   resumeWorkflowFromHumanResponses,
   type HumanResponseEventData,
 } from './hitl/index'
+import type { UIMessageStreamWriter } from 'ai'
 
+/**
+ * Run a workflow with user input and return a workflow context
+ */
 export async function runWorkflow({
   workflow,
   input,
@@ -58,6 +66,89 @@ export async function runWorkflow({
   }
 
   return context
+}
+
+export interface WorkflowStreamCallbacks {
+  onStart?: (writer: UIMessageStreamWriter) => Promise<void> | void
+  onFinal?: (
+    completion: string,
+    writer: UIMessageStreamWriter
+  ) => Promise<void> | void
+  onText?: (text: string, writer: UIMessageStreamWriter) => Promise<void> | void
+  onPauseForHumanInput?:
+    | ((event: WorkflowEvent<HumanResponseEventData>) => Promise<void> | void)
+    | undefined
+}
+
+const encoder = new TextEncoder()
+
+function toSSE(chunk: any) {
+  return encoder.encode(`data: ${JSON.stringify(chunk)}\n\n`)
+}
+
+export function llamaindexWorkflowEventsToSSE(
+  // TODO: handle callbacks
+  callbacks?: WorkflowStreamCallbacks
+) {
+  return new TransformStream({
+    start(controller) {
+      // onStart
+    },
+    async transform(event, controller) {
+      // agentStreamEvent -> send a text-delta to stream
+      // TODO: need agentStreamStartEvent, agentStreamEndEvent
+      // TODO: need id to group agentStreamEvent, agentStreamStartEvent, agentStreamEndEvent
+      // TODO: make a TextPartTransformer pipe?
+      if (agentStreamEvent.include(event)) {
+        const { delta, currentAgentName } = event.data
+        const textDeltaPart: TextDeltaPart = {
+          id: currentAgentName,
+          type: 'text-delta',
+          delta,
+        }
+        controller.enqueue(toSSE(textDeltaPart))
+      }
+
+      // agentToolCallEvent -> send a running event to stream
+      // TODO: make a RunningEventTransformer pipe?
+      if (agentToolCallEvent.include(event)) {
+        const { agentName, toolName, toolKwargs, toolId } = event.data
+        const runEventPart: EventPart = {
+          // TODO: maybe toolCallId is better, because 1 tool can be called multiple times
+          id: toolName, // use toolName as id to reconciliation with agentToolCallResultEvent
+          type: 'data-event',
+          data: {
+            title: `Agent Tool Call: ${agentName}`,
+            description: `Using tool: '${toolName}' with inputs: '${JSON.stringify(toolKwargs)}'`,
+            status: 'pending',
+            data: event.data,
+          },
+        }
+        controller.enqueue(toSSE(runEventPart))
+      }
+
+      // agentToolCallResultEvent -> send a running event with data to stream
+      if (agentToolCallResultEvent.include(event)) {
+        const { raw, toolName, toolKwargs, toolOutput, toolId } = event.data
+        const dataEventPart: EventPart = {
+          id: toolName, // use toolName as id to reconciliation with agentToolCallEvent
+          type: 'data-event',
+          data: {
+            title: `Agent Tool Call Result: ${toolName}`,
+            description: `Using tool: '${toolName}' with inputs: '${JSON.stringify(toolKwargs)}'`,
+            status: 'success',
+            data: raw,
+          },
+        }
+        controller.enqueue(toSSE(dataEventPart))
+      }
+
+      // if detect source nodes, send source event at the end of stream, also download files in background
+    },
+    flush(controller) {
+      // onFinal
+    },
+  })
 }
 
 export function processWorkflowStream(
